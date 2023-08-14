b5b7960db2d2584698fab23b58c7b77bd212e379
quackquavk <138971300+quackquavk@users.noreply.github.com>
2023-08-14 22:11:18 +0545

    Add files via upload

---
+++ workoot/src/screens/Desktop/Desktop.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import "./style.css";
+
+export const Desktop = () => {
+  return (
+    <div className="desktop">
+      <div className="div">
+        <div className="overlap">
+          <img className="rectangle" alt="Rectangle" src="/img/rectangle-1.png" />
+          <div className="frame">
+            <div className="frame-wrapper">
+              <div className="div-wrapper">
+                <div className="fight-like-a-wrapper">
+                  <p className="fight-like-a">
+                    <span className="text-wrapper">Fight like  a </span>
+                    <span className="span">champion </span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <img className="play" alt="Play" src="/img/play.svg" />
+          <div className="BO-xchamp">BOXCHAMP</div>
+          <div className="navbar">
+            <div className="text-wrapper-2">HOME</div>
+            <div className="text-wrapper-2">ABOUT</div>
+            <div className="text-wrapper-2">LEARN</div>
+            <div className="text-wrapper-3">HAHA</div>
+            <div className="frame-2">
+              <div className="sign-up-wrapper">
+                <div className="sign-up">SIGN UP</div>
+              </div>
+            </div>
+          </div>
+          <div className="frame-3">
+            <div className="div-wrapper">
+              <div className="frame-4">
+                <img className="image" alt="Image" src="/img/image-3.png" />
+                <div className="gym">GYM</div>
+              </div>
+            </div>
+            <div className="frame-5">
+              <img className="img" alt="Image" src="/img/image-4.png" />
+              <div className="home-workout">HOME WORKOUT</div>
+            </div>
+            <div className="frame-6">
+              <img className="image-2" alt="Image" src="/img/image-2.png" />
+              <div className="m-editation">MEDITATION</div>
+            </div>
+          </div>
+        </div>
+        <div className="overlap-group">
+          <div className="rectangle-2" />
+          <div className="frame-7">
+            <div className="text-wrapper-4">Privacy policy</div>
+          </div>
+          <div className="terms-conditions">Terms &amp; conditions</div>
+          <div className="frame-8">
+            <img className="image-3" alt="Image" src="/img/image-1.png" />
+            <div className="text-wrapper-5">pukarkhanal5@gmail.com</div>
+          </div>
+          <div className="frame-9">
+            <p className="ready-for-your-next">
+              <span className="text-wrapper-6">
+                Ready for your <br />
+              </span>
+              <span className="span">next</span>
+              <span className="text-wrapper-6"> lesson?</span>
+            </p>
+            <img className="frame-10" alt="Frame" src="/img/frame-14.svg" />
+          </div>
+        </div>
+        <img className="rectangle-3" alt="Rectangle" src="/img/rectangle-3.png" />
+        <img className="boy-fight" alt="Boy fight" src="/img/boy-fight-1.png" />
+        <p className="HOW-WE-GOT-OUR">
+          HOW WE GOT OUR <br />
+          BUSINESS STARTED?
+        </p>
+        <p className="lorem-ipsum-dolor">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit.Resse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occa
+          <br />
+          <br />
+          ecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <img className="rectangle-4" alt="Rectangle" src="/img/rectangle-4.svg" />
+      </div>
+    </div>
+  );
+};
